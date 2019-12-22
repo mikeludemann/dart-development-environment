ad55350c10f96e5005fbcf4c20fbb6323518591a
Mike Ludemann <mike-ludemann@hotmail.de>
2019-12-22 13:00:44 +0100

    Setup complete - dart-development-environment

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_development_environment/dart_development_environment.dart' as dart_development_environment;
+
+main(List<String> arguments) {
+  print('Hello world: ${dart_development_environment.calculate()}!');
+}
---
+++ test/dart_development_environment_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_development_environment/dart_development_environment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
